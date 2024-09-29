f30867432c2b73407c9dcc88daaa6f03379f7cae
Brian Jolly <brian@brianjolly.com>
2024-09-28 22:36:26 -0700

    Move sliders out of flexbox

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -8,6 +8,7 @@
 
 #include "PluginProcessor.h"
 #include "juce_audio_processors/juce_audio_processors.h"
+#include "juce_graphics/juce_graphics.h"
 #include "PluginEditor.h"
 
 //==============================================================================
@@ -16,18 +17,21 @@ ScopeAudioProcessorEditor::ScopeAudioProcessorEditor (ScopeAudioProcessor& p)
 {
     addAndMakeVisible(audioProcessor.waveViewer);
     audioProcessor.waveViewer.setColours(juce::Colours::black, juce::Colours::whitesmoke.withAlpha(0.5f));
+    //audioProcessor.waveViewer.setTransform(juce::AffineTransform::rotation(90.f));
 
     addAndMakeVisible(waveRepaintRateSlider);
     waveRepaintRateSlider.setRange (2, 2048);
     waveRepaintRateSlider.setValue (2);
+    waveRepaintRateSlider.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
     waveRepaintRateSlider.addListener (this);
 
     addAndMakeVisible(waveBufferSizeSlider);
     waveBufferSizeSlider.setRange (2, 2048);
     waveBufferSizeSlider.setValue (512);
+    waveBufferSizeSlider.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
     waveBufferSizeSlider.addListener (this);
 
-    setSize (400, 300);
+    setSize (500, 500);
 }
 
 ScopeAudioProcessorEditor::~ScopeAudioProcessorEditor()
@@ -50,8 +54,12 @@ void ScopeAudioProcessorEditor::resized()
     juce::Rectangle<int> bounds = getLocalBounds();
     juce::FlexBox flexBox;
 
-    flexBox.items.add(juce::FlexItem (waveBufferSizeSlider).withMinHeight(50.0f).withMinWidth(50.0f).withFlex(1));
-    flexBox.items.add(juce::FlexItem (waveRepaintRateSlider).withMinHeight(50.0f).withMinWidth(50.0f).withFlex(1));
+    const int border = 120;
+
+    waveRepaintRateSlider.setBounds(border, 20, getWidth() - border, 20);
+    waveBufferSizeSlider.setBounds(border, 50, getWidth() - border, 50);
+    //flexBox.items.add(juce::FlexItem (waveBufferSizeSlider).withMinHeight(50.0f).withMinWidth(50.0f).withFlex(1));
+    //flexBox.items.add(juce::FlexItem (waveRepaintRateSlider).withMinHeight(50.0f).withMinWidth(50.0f).withFlex(1));
     flexBox.items.add(juce::FlexItem (audioProcessor.waveViewer).withMinHeight(50.0f).withMinWidth(50.0f).withFlex(1));
 
     flexBox.performLayout(bounds);
